4b60bd6fb8dad4f612297b103ece55831cf8ced3
Saima Fathima <saima.fathi2011@gmail.com>
2024-11-28 16:42:06 +0530

    datatype

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+name = "penguin"
+age=13
+is_student=True
+weight=37.9
+print("Name:",name)
+print("data type name is",type(name))
+print("age:",age)
+print("data type age is",type(age))
+print("is_student:",is_student)
+print("data type is_student is",type(is_student))
+print("weight:",weight)
+print("data type weight is",type(weight))
+
+print("\n After type casting...." )
+age=str(age)
+print(age)
+print("data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight is", type(weight))
