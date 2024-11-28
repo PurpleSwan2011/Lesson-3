deb3f1d09f72c8aa8f77ddb9d60179e425747a7a
Saima Fathima <saima.fathi2011@gmail.com>
2024-11-28 16:59:26 +0530

    pro1

---
+++ pro1.py
@@ -0,0 +1,9 @@
+x="coNgrAtuLatiOns"
+print(str.lower(x))
+print(str.upper(x))
+print("The first string: ")
+strOne=input()
+print("The second string: ")
+strTwo=input()
+strThree=strOne +" " +strTwo
+print("\n concatenated string",strThree)
